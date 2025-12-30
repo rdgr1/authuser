c34bca6d1ba71043e1b38b9653e21e4ef56e57dc
rdgr1 <rodgerdias6@gmail.com>
2025-12-30 11:43:21 -0300

    feat: implement cors registry configuration in level of class, and implemente cors registry global, with webMvcConfig

--- src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
@@ -17,4 +18,8 @@ public class ResolverConfig implements WebMvcConfigurer {
         argumentResolver.add(pageableResolver);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/users/**").allowedOrigins("*");
+     }
 }
